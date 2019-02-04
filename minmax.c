563f9d6a548f8f0b01d54d75235ddd995f550c43
keerthana-golusula <keerthanag9298@gmail.com>
2019-02-04 15:20:02 -0800

    added minmax program

---
+++ minmax.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main() {
+	int arr[100];
+	int i = 0, sum = 0, min = 36555, max = 0, l = 0;
+	float avg;
+	while (1) {
+		scanf("%d", &arr[i]);
+		if (arr[i] <= 0) {
+			break;
+		}
+		else {
+			i++;
+			l = i;
+		}
+	}
+	for (i = 0; i <l; i++) {
+		if (arr[i] < min)
+			min = arr[i];
+		if (arr[i] > max)
+			max = arr[i];
+		sum = sum + arr[i];
+		
+	}
+	avg = (float)(sum / l);
+	printf("minimum value is %d", min);
+	printf("maximum value is %d", max);
+	printf("average value is %f",avg);
+
+}
