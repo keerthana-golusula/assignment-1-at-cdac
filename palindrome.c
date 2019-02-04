7bf1b41ef7d6c399db38d1a1cb60d1a258ecf919
keerthana-golusula <keerthanag9298@gmail.com>
2019-02-04 14:15:51 -0800

    Added palindrome program

--- palindrome.c
+++ palindrome.c
@@ -1,1 +1,20 @@
 #include<stdio.h>
+int main()
+{
+	int num,n,rev=0,rem=0;
+	scanf("%d", &num);
+	n = num;
+	while (n) {
+		rem = n % 10;
+		rev = rev * 10 + rem;
+		n = n / 10;
+	}
+	if (rev == num) 
+		printf("given number is palindrome");
+	else 
+		printf("given number is not a palindrome");
+	int x;
+	scanf("%d", &x);
+
+	return 0;
+}
